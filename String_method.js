23f9f2467a4bc707c9f438cbc919aa272d0e9957
Parth Sishodia <91628095+parthsishodia@users.noreply.github.com>
2023-04-08 19:46:56 +0530

    Create String_method.js

---
+++ String_method.js
@@ -0,0 +1,30 @@
+ let name="Parth\""
+ console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("Par","har"))
+
+
+
+
+let name1="Naman"
+let name2="Harry"
+console.log(name1.concat(name2))
+let frnd="             Parth    "
+console.log(frnd.trim())
+// console.log(frnd) o/p:             Parth
+
+
+console.log(frnd.padStart(5,0))
+
+let str="Apple,Banana,Kiwi"
+let part =str.substring(1,6)
+console.log(part)
+
+let text= "5";
+let padded=text.padStart(7,".")
+console.log(padded)
+
+
