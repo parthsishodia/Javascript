6c2aabe26e78c9d4f9b9baf98608e174cc878a54
Parth Sishodia <91628095+parthsishodia@users.noreply.github.com>
2023-04-08 19:45:56 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,29 @@
+let name="Parth"
+console.log(name.length)
+let frnd="Prakash"
+console.log(frnd)
+let myfrnd='Shubh'
+console.log(myfrnd)
+console.log(name[1])
+
+
+//Template literals
+
+let boy1="Pramod"
+let boy2="Nikhil"
+let sentence=`${boy1} is friend of ${boy2}`
+console.log(sentence)
+
+
+//Escape sequence character
+
+let fruit="Bana\"\nna"
+console.log(fruit)
+
+//Use for loop to print a string 
+ let fri="Pranav"
+ for(let i in fri)
+ {
+    console.log(fri[i])
+ }
+
