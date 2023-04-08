9562f90f1eb5b7fe873a641ca70e1f5026811f1a
Parth Sishodia <91628095+parthsishodia@users.noreply.github.com>
2023-04-08 19:48:13 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+// NN BB SS U --Primitive in js
+let a =null;//Varibale that is nothing
+let b=456;
+let c=true;//Can also be false
+let d=BigInt("789") + BigInt("78")
+let e="Parth"
+let f=Symbol("I am Parth")
+let g=undefined//Its means that nothing is given to it.
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c)
+//Objects in Js(NON-PRIMITIVE)
+const item={
+    "Parth":true, 
+    "Utkarsh":false,
+    "Love":69,
+    "Rohan":undefined
+}
+console.log(item["Love"])
+
